dc50bd93da305d517e3de2b07664f6ebf2eb133d
James Broadhead <jbroadhead@twitter.com>
2014-10-28 15:12:24 +0000

    Bundle for distribution -- switch to setuptools for bdist_egg

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='csputils',
-    version='0.1',
+    version='0.1.1',
     description='csputils - helpers for writing Content-Security-Policy rules',
     author='jbroadhead',
     author_email='jbroadhead@twitter.com',
     url='https://github.com/jamesbroadhead/csputils.git',
     packages = ['csputils'],
-    package_dir = {'': ''},
 )
